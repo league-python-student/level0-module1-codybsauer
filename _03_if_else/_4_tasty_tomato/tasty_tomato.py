cdfc8cfe7bd1cc708c9eeb5f6477ef6ed1bf6f30
codysauer <cody.sauer@jointheleague.org>
2021-09-22 20:31:21 +0000

    test

--- _03_if_else/_4_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_4_tasty_tomato/tasty_tomato.py
@@ -11,7 +11,7 @@ canvas.grid()
 #1. Ask the user what color tomato they would like and save their response   
 #   You can give them up to three choices 
 
-
+#test
 #2. use if-else statements to draw the tomato in the color that they chose
 #   you can modify the code below or draw your own tomato
 canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
